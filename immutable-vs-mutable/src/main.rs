910005153f10aa4a0985f3bcc6fa4a7d86a61350
makufff <Tanapatx.tyran@gmail.com>
2025-01-28 18:57:25 +0700

    Course : Immutable-vs-mutable(🦀)

---
+++ immutable-vs-mutable/src/main.rs
@@ -0,0 +1,11 @@
+fn main() {
+
+    // let crabby_energy = 0 ;
+    // crabby_energy = crabby_energky + 1;
+    // help: a local variable with a similar name exists: `crabby_energy`
+
+    let mut crabby_energy = 0 ;
+    crabby_energy = crabby_energy + 1;
+
+    println!("Hello, world! {}" , crabby_energy);
+}
